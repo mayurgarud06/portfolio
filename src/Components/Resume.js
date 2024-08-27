d64e849c0e798b2269edb8062c2bc13eae0f2ea3
mayurgarud06 <mayurgarud06@gmail.com>
2024-08-28 03:02:13 +0530

    adding changes

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,11 +5,9 @@ import Images from '../static/Images.js';
 
 const Resume = () =>{
   
-   const resumeLink ='https://docs.google.com/document/d/1kZ-Tvl1qvZ9unx0JX91519-Zsc3kNlGFLmZJomGKupg/pub?embedded=true';
-  const downloadLink='https://docs.google.com/document/d/1kZ-Tvl1qvZ9unx0JX91519-Zsc3kNlGFLmZJomGKupg/export?format=pdf'
-  //  const resumeLink="https://pdf.ac/1s5GZh";
-  // const resumeLink="abc.pdf";
-
+   const resumeLink ="https://docs.google.com/document/d/1kZ-Tvl1qvZ9unx0JX91519-Zsc3kNlGFLmZJomGKupg/pub?embedded=true";
+  const downloadLink="https://docs.google.com/document/d/1kZ-Tvl1qvZ9unx0JX91519-Zsc3kNlGFLmZJomGKupg/export?format=pdf";
+ 
   const handleDownload = () => {
     // Trigger download
     const link = document.createElement('a');
@@ -23,7 +21,6 @@ const Resume = () =>{
     <div className="resume-container">
       <h2 className='resume-title'> My Resume</h2>
       <div className="iframe-container">
-         {/* Resume Preview */}
         <iframe
           src={resumeLink}
           title="Resume Preview"
@@ -33,28 +30,28 @@ const Resume = () =>{
         ></iframe>
       </div> 
     </div>
-     {/* Download Button */}
+     
      <button onClick={handleDownload} className="download-button">
         Download Resume
       </button>
     <h1> Skills...</h1>
    <div className='skill_container'>
-    <div className='skills'>
-      <div className="skill-item"><img src={Images.Java}/><p>Java</p></div>
-      <div className="skill-item"><img src={Images.python}/><p>Python</p></div>
-      <div className="skill-item"><img src={Images.Mysql}/><p>MySQL</p></div>
-      <div className="skill-item"><img src={Images.reactLogo}/><p>React.JS</p></div>
-      <div className="skill-item"><img src={Images.springboot}/><p>Spring Boot</p></div>
-      <div className="skill-item"><img src={Images.git}/><p>GIT</p></div>
-      <div className="skill-item"><img src={Images.github}/><p>GitHub</p></div>
-      <div className="skill-item"><img src={Images.JavaScript}/><p>JavaScript</p></div>
-      <div className="skill-item"><img src={Images.html}/><p>HTML</p></div>
-      <div className="skill-item"><img src={Images.gcp}/><p>Google Cloud Platform</p></div>
-      <div className="skill-item"><img src={Images.docker}/><p>Docker Container</p></div>
-      <div className="skill-item"> <img src={Images.api}/><p>Rest API</p></div>
-      <div className="skill-item"><img src={Images.swagger}/><p>swagger Documentation API</p></div>
-      <div className="skill-item"><img src={Images.postman}/><p>Postman API</p></div>
-    </div>   
+      <div className='skills'>
+        <div className="skill-item"><img src={Images.Java}/><p>Java</p></div>
+        <div className="skill-item"><img src={Images.python}/><p>Python</p></div>
+        <div className="skill-item"><img src={Images.Mysql}/><p>MySQL</p></div>
+        <div className="skill-item"><img src={Images.reactLogo}/><p>React.JS</p></div>
+        <div className="skill-item"><img src={Images.springboot}/><p>Spring Boot</p></div>
+        <div className="skill-item"><img src={Images.git}/><p>GIT</p></div>
+        <div className="skill-item"><img src={Images.github}/><p>GitHub</p></div>
+        <div className="skill-item"><img src={Images.JavaScript}/><p>JavaScript</p></div>
+        <div className="skill-item"><img src={Images.html}/><p>HTML</p></div>
+        <div className="skill-item"><img src={Images.gcp}/><p>Google Cloud Platform</p></div>
+        <div className="skill-item"><img src={Images.docker}/><p>Docker Container</p></div>
+        <div className="skill-item"> <img src={Images.api}/><p>Rest API</p></div>
+        <div className="skill-item"><img src={Images.swagger}/><p>swagger Documentation API</p></div>
+        <div className="skill-item"><img src={Images.postman}/><p>Postman API</p></div>
+      </div>   
     </div>
     
   </div>
